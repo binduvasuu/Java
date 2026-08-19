7fa4427667f6cc45830f7eef8af1ed5417c25e27
bindu <bindupoorvi11@gmail.com>
2026-08-19 12:37:40 +0530

    association_airport

---
+++ Airport.java
@@ -0,0 +1,16 @@
+class Airport{
+
+	int airportId;
+	String airportName;
+	String airportLocation;
+
+	Terminal terminal;
+
+	public void diplayAirportDetails(){
+		System.out.println("the airport id is: "+ airportId);
+		System.out.println("the airport name is: "+ airportName);
+		System.out.println("the aiprort location is: "+ airportLocation);
+
+	terminal.displayTerminalDetails();
+	}
+}
